2c2402b82cdcc13bb40aba51f0d555f75275ff7b
Kaijun <Demonbane.cn@gmail.com>
2015-06-19 01:24:19 +0200

    fix

--- app/scripts/landing-page.js
+++ app/scripts/landing-page.js
@@ -7,7 +7,7 @@
 
 	var bannerSlider = $('.lp-banner').unslider({
 		speed: 500,               //  The speed to animate each slide (in milliseconds)
-		delay: 10000003000,              //  The delay between slide animations (in milliseconds)
+		delay: 3000,              //  The delay between slide animations (in milliseconds)
 		complete: function() {},  //  A function that gets called after every slide animation
 		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
 		dots: true,               //  Display dot navigation
