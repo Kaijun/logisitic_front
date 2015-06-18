2d58a7b37923d66e5cd063ae47f9f4fd3b03b684
Kaijun <Demonbane.cn@gmail.com>
2015-06-19 00:26:36 +0200

    index perfect

--- app/scripts/landing-page.js
+++ app/scripts/landing-page.js
@@ -5,4 +5,13 @@
 		$(this).toggleClass('x');
 	});
 
+	var bannerSlider = $('.lp-banner').unslider({
+		speed: 500,               //  The speed to animate each slide (in milliseconds)
+		delay: 10000003000,              //  The delay between slide animations (in milliseconds)
+		complete: function() {},  //  A function that gets called after every slide animation
+		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+		dots: true,               //  Display dot navigation
+		fluid: true              //  Support responsive design. May break non-responsive designs
+	});
+
 })(this, jQuery);
