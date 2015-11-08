7424467ceb0bdc9097699dd38167875ebc5dd230
Kaijun <Demonbane.cn@gmail.com>
2015-11-08 18:03:46 +0100

    用户界面修改默认地址

--- app/home-app/controllers/profile/PersonalInfo.js
+++ app/home-app/controllers/profile/PersonalInfo.js
@@ -19,18 +19,28 @@
             "ID_card_scan_1": UserInfo.ID_card_scan_1,
             "ID_card_scan_2": UserInfo.ID_card_scan_2,
             "real_name": UserInfo.real_name,
+            "default_address_id": null,
         }
 
+        $scope.addrs = [];
+        $scope.defaultAddr = null;
         activate();
 
         ////////////////
 
         function activate() {
 
+            ProfileService.getAddressList().then(function (data) {
+                $scope.addrs = data;
+                $scope.defaultAddr = $scope.addrs.filter(function (item) {
+                    return parseInt(item.is_default) === 1;
+                })[0];
 
+            })
         }
 
         $scope.editProfile = function () {
+            $scope.personalInfo.default_address_id = $scope.defaultAddr.id;
             ProfileService.editPersonalInfo($scope.personalInfo).then(function () {
                 swal({
                     type: "success",
