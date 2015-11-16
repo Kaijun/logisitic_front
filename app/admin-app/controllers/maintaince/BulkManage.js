279897a2d3decf158798a6b050916a095f7f3f3f
Kaijun <Demonbane.cn@gmail.com>
2015-11-16 17:06:50 +0100

    群发消息-选择多个用户

--- app/admin-app/controllers/maintaince/BulkManage.js
+++ app/admin-app/controllers/maintaince/BulkManage.js
@@ -9,6 +9,9 @@
 
     /* @ngInject */
     function BulkManage($scope, $timeout, MessageService, $state, RoleService) {
+
+        var TIMEOUT_DELAY = 500;
+
         $scope.bulkList = [];
         
         $scope.isPopupShown = false;
@@ -18,6 +21,9 @@
         $scope.isNotification = false;
         $scope.isEmail = false;
         $scope.isSMS = false;
+        $scope.select = {
+            sendByRole: 1,
+        };
         $scope.popupConfirm = popupConfirm;
         $scope.popupCancle = popupCancle;
         activate();
@@ -36,6 +42,72 @@
                     $scope.roleChosen = $scope.roles[0];
                 })
             });
+
+            $scope.$watch('select.sendByRole', function (newValue, oldValue) {
+                if(newValue==0){
+                    //selectize
+                    var searchUserTimeout;
+                    $timeout(function () {
+                        $('#user-tags').selectize({
+                            persist: false,
+                            maxItems: null,
+                            valueField: 'id',
+                            labelField: 'name',
+                            searchField: ['name'],
+                            searchConjunction: 'or',
+                            options: [
+                                {id: 1, name: 'Brian Reavis', email: "123@gmail.com"},
+                                {id: 2, name: 'Kaijun Chen', email: "123@gmail.com"},
+                                {id: 3, name: 'Pu Junyu', email: "123@gmail.com"},
+                                {id: 4, name: 'Hehehu', email: "123@gmail.com"},
+                                {id: 5, name: 'Sun Jun', email: "123@gmail.com"},
+                            ],
+                            render: {
+                                item: function(item, escape) {
+                                    return '<div>' +
+                                        (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+                                        (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+                                    '</div>';
+                                },
+                                option: function(item, escape) {
+                                    var label = item.name || item.email;
+                                    var caption = item.name ? item.email : null;
+                                    return '<div>' +
+                                        '<span class="label">' + escape(label) + '</span>' +
+                                        (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+                                    '</div>';
+                                }
+                            },
+                            onType: function (input) {
+                                var self = this;
+                                if (searchUserTimeout) $timeout.cancel(searchUserTimeout);
+                                searchUserTimeout = $timeout(function() {
+                                    // search();
+                                    var options = [
+                                        {id: 7, name: 'etwas anderes', email: "123@gmail.com"},
+                                        {id: 8, name: 'etwas anderes', email: "123@gmail.com"},
+                                        {id: 9, name: 'etwas anderes', email: "123@gmail.com"},
+                                        {id: 10, name: 'etwas anderes', email: "123@gmail.com"},
+                                        {id: 11, name: 'etwas anderes', email: "123@gmail.com"},
+                                    ];
+                                    self.addOption(options);
+                                    self.refreshOptions(true);
+
+
+
+                                }, TIMEOUT_DELAY); 
+                            },
+
+                        });
+                        
+                    })
+                }
+                else if(newValue == 1){
+
+                }
+            })
+            
+            
         }
 
         function popupConfirm () {
@@ -44,8 +116,9 @@
                 return;
             }
             MessageService.sendBulkMsg({
-                send_by_role:1,
-                roles:[$scope.roleChosen.id], 
+                send_by_role: $scope.select.sendByRole,
+                roles: $scope.select.sendByRole==1?[]:[], 
+                users: $scope.select.sendByRole==0?[]:[], 
                 content: $scope.textContent, 
                 send_notification: $scope.isNotification, 
                 send_email: $scope.isEmail,
