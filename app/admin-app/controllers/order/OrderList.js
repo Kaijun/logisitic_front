882ad180f3722bacde81845e281131419261b00a
Kaijun <Demonbane.cn@gmail.com>
2016-03-15 18:01:58 +0100

    no message

--- app/admin-app/controllers/order/OrderList.js
+++ app/admin-app/controllers/order/OrderList.js
@@ -240,7 +240,7 @@
 
             function downloadCSV(filename, text) {
               var element = document.createElement('a');
-              element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+              element.setAttribute('href', 'data:application/csv;charset=utf-8,' + encodeURIComponent(text));
               element.setAttribute('download', filename);
 
               element.style.display = 'none';
