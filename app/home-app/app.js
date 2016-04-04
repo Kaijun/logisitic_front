fe9b0d957ac49157f322d9cd325d4dcc92d43eb5
Kaijun <Demonbane.cn@gmail.com>
2016-04-04 20:39:44 +0200

    fix removal test data

--- app/home-app/app.js
+++ app/home-app/app.js
@@ -213,11 +213,11 @@
 
 
 
-    var user  = {"id":2,"name":"xrwr","email":"xrwr@qq.com","updated_at":"2016-04-03 22:13:05","created_at":"2015-10-23 13:44:09","stock_number":"IHYBTL","stock_position":10001,"role_id":1,"remain":100000.0078125,"score":10000000,"exp":10000000,"QQ":57826867,"wechat":"weqweqwe","phone_number":"13505876544","ID_card_number":"321321312321344","ID_card_scan_1":"","ID_card_scan_2":"","real_name":"pjy","is_deletable":0,"is_assured":0,"usable_score":0,"config":{"score_money_ratio":5},"role":{"id":1,"role_name":"\u5ba2\u6237","is_backend":0,"accountancy":0,"stock_manage":0,"ship_manage":0,"is_adminstrator":0,"is_vip":0,"updated_at":"-0001-11-30 00:00:00","created_at":"-0001-11-30 00:00:00","is_deletable":0}};
-    user.level = {"id":25,"level_name":"\u666e\u901a\u7528\u6237","lower_bound":200,"usable_amount":0,"usable_ratio":"0.000","logistic_paths":[{"id":10,"name":"\u79fb\u5e93\u8def\u7ebf","ship_company_international":"DKD","ship_company_china":"YK","type":2,"base_price":10,"description":"\u79fb\u5e93\u9ed8\u8ba4\u7ebf\u8def\uff0c\u4e0d\u53ef\u5220\u9664","weight_upper_bound":30,"user_group":0,"based_on":0,"created_at":null,"updated_at":"2015-10-20 14:20:29","is_deletable":0,"extra_services":[],"pivot":{"user_level_id":25,"logistic_path_id":10,"is_usable":1,"discount_ratio":"0.000"}}],"extra_services":[]};
-    user.unread_conversation = 0;
-    user.unread_notification = 4;
-    angular.module('home').constant('UserInfo', user);
+    // var user  = {"id":2,"name":"xrwr","email":"xrwr@qq.com","updated_at":"2016-04-03 22:13:05","created_at":"2015-10-23 13:44:09","stock_number":"IHYBTL","stock_position":10001,"role_id":1,"remain":100000.0078125,"score":10000000,"exp":10000000,"QQ":57826867,"wechat":"weqweqwe","phone_number":"13505876544","ID_card_number":"321321312321344","ID_card_scan_1":"","ID_card_scan_2":"","real_name":"pjy","is_deletable":0,"is_assured":0,"usable_score":0,"config":{"score_money_ratio":5},"role":{"id":1,"role_name":"\u5ba2\u6237","is_backend":0,"accountancy":0,"stock_manage":0,"ship_manage":0,"is_adminstrator":0,"is_vip":0,"updated_at":"-0001-11-30 00:00:00","created_at":"-0001-11-30 00:00:00","is_deletable":0}};
+    // user.level = {"id":25,"level_name":"\u666e\u901a\u7528\u6237","lower_bound":200,"usable_amount":0,"usable_ratio":"0.000","logistic_paths":[{"id":10,"name":"\u79fb\u5e93\u8def\u7ebf","ship_company_international":"DKD","ship_company_china":"YK","type":2,"base_price":10,"description":"\u79fb\u5e93\u9ed8\u8ba4\u7ebf\u8def\uff0c\u4e0d\u53ef\u5220\u9664","weight_upper_bound":30,"user_group":0,"based_on":0,"created_at":null,"updated_at":"2015-10-20 14:20:29","is_deletable":0,"extra_services":[],"pivot":{"user_level_id":25,"logistic_path_id":10,"is_usable":1,"discount_ratio":"0.000"}}],"extra_services":[]};
+    // user.unread_conversation = 0;
+    // user.unread_notification = 4;
+    // angular.module('home').constant('UserInfo', user);
     
     
     
